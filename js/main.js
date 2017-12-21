781badf320bc449e079cf7403daa4d36c4595665
co2co2 <coco8287@hotmail.com>
2017-12-21 15:49:10 -0500

    switch done learned classlist add and remove.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+  var button = document.querySelector('button');
+  var status = document.querySelector('.status')
+  var shout = document.querySelector('h1')
 
 
+  button.addEventListener('click',function(){
+  if (button.classList.contains("on")){
+    shout.innerText = 'Hey, who turned off the lights?'
+    document.body.classList.add("dark")
+    button.classList.remove("on")
+    button.classList.add("off")
+  } else {
+    shout.innerText = 'It s so bright in here!'
+    document.body.classList.remove("dark")
+    button.classList.remove("off")
+    button.classList.add("on")
+  }
+
+
+  });
+})
